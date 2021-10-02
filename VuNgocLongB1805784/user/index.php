62810e117a28bd2297cb1a3b802185e4a0d8d300
VuNgocLong2626 <long.speed00@gmail.com>
2021-10-02 17:00:38 +0700

    template

---
+++ VuNgocLongB1805784/user/index.php
@@ -0,0 +1,24 @@
+<?php 
+	include "../admin/process/mysql_connect.php"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if ($conn -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+            exit();
+          }
+        else{
+            echo"Ket noi thanh cong";
+        }
+    ?>
+</body>
+</html>
